5a150ca1df730500126c72708a9e314cfb7d7763
Yosef Benny Widyokarsono <yosef@operativ.sg>
2020-04-12 01:06:47 +0700

    Create geocoder initializers: - add api_key

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,22 @@
+Geocoder.configure(
+
+  # street address geocoding service (default :nominatim)
+  #lookup: :yandex,
+
+  # IP address geocoding service (default :ipinfo_io)
+  #ip_lookup: :maxmind,
+
+  # to use an API key:
+  api_key: "AIzaSyAf6udEq_mNK0HhwhHmCin_Id_1GyNEKKg",
+
+  # geocoding service request timeout, in seconds (default 3):
+  #timeout: 5,
+
+  # set default units to kilometers:
+  #units: :km,
+
+  # caching (see [below](#caching) for details):
+  #cache: Redis.new,
+  #cache_prefix: "..."
+
+)
